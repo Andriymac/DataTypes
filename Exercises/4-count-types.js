0e71fb4614538c7c077c59017becf91f20d0ca1f
Andriymac <macho255@ukr.net>
2020-01-04 16:46:49 +0200

    made the last exercise 4. It's perfect :)

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const countTypesInArray = null;
+const countTypesInArray = arr => {
+  const coll = {};
+  for (const elem of arr) {
+    const state = typeof elem;
+    coll[state] !== undefined ? coll[state] += 1 : coll[state] = 1;
+  }
+  return coll;
+};
 
 module.exports = { countTypesInArray };
